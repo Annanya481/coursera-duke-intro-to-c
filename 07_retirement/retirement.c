11a8442b0cd902467fa3d6f404d5e63ea57e9a45
Coursera Learner <nobody@nowhere.nul>
2020-05-13 10:46:33 +0000

    Completed retirement assignment

--- 07_retirement/retirement.c
+++ 07_retirement/retirement.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct _retire_info {
+struct _retire_info {
   int months;
   double contribution;
   double rate_of_return;
-}retire_info;
+};
+
+typedef struct _retire_info retire_info;
 
 double next_balance(double balance, retire_info ri){
   return balance * (1 + ri.rate_of_return) + ri.contribution;
