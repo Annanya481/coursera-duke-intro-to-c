bb146e364aa09f75980dd1a775198c907249e0fd
Coursera Learner <nobody@nowhere.nul>
2020-05-25 05:00:28 +0000

    Completed deck

--- c3prj1_deck/deck.c
+++ c3prj1_deck/deck.c
@@ -6,7 +6,7 @@ void print_hand(deck_t * hand){
   card_t ** ptr = hand->cards;
   for(int i=0; i<(hand->n_cards); i++){
     print_card(**ptr);
-    printf("%s", " ");
+    printf("%s"," ");
     ptr++;
   }
 }
@@ -14,7 +14,7 @@ void print_hand(deck_t * hand){
 int deck_contains(deck_t * d, card_t c) {
   card_t ** ptr = d->cards;
   for(int i=0; i<(d->n_cards); i++){
-    if(suit_letter(**ptr) == suit_letter(c) && value_letter(**ptr) == value_letter(c)){
+    if(suit_letter(**ptr)== suit_letter(c) && value_letter(**ptr)==value_letter(c)){
       return 1;
     }
     ptr++;
@@ -40,15 +40,16 @@ void shuffle(deck_t * d){
 void assert_full_deck(deck_t * d) {
   card_t ** ptr = d->cards;
   deck_t temp_deck;
-  temp_deck.cards = d-><cards;
+  temp_deck.cards = d->cards;
   for(int i=0; i<(d->n_cards); i++){
     card_t temp_card = **ptr;
     assert_card_valid(temp_card);
     if(i>0){
       temp_deck.n_cards = (size_t)i;
-      assert(!deck_contains(&temp_deck, temp_cards));
+      assert(!deck_contains(&temp_deck, temp_card));
     }
     ptr++;
+  }
 }
 
   void add_card_to(deck_t * deck, card_t c){
