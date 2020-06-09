35bb85b058249844b950b7da3f00afe2941a82df
Coursera Learner <nobody@nowhere.nul>
2020-06-09 13:51:12 +0000

    Completed

--- 29_outname/outname.c
+++ 29_outname/outname.c
@@ -5,4 +5,23 @@
 
 char * computeOutputFileName(const char * inputName) {
   //WRITE ME
+  int len = strlen(inputName);
+  char * outNameptr = malloc((len+8) * sizeof(*outNameptr));
+  char *ptr = outNameptr;
+
+  while(*inputName != '\0'){
+    *ptr = *inputName;
+    ptr++;
+    inputName++;
+  }
+
+  char *suffix = ".counts";
+  while(*suffix != '\0'){
+    *ptr = *suffix;
+    ptr++;
+    suffix++;
+  }
+  *ptr = '\0';
+
+  return outNameptr;
 }
